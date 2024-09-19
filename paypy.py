6cf23daa8a07a03efb7201742646608d8635dcec
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-09-19 16:23:51 +0200

    paypy go back

--- paypy.py
+++ paypy.py
@@ -4,58 +4,37 @@ from socket import socket, AF_INET, SOCK_DGRAM
 from colorama import Fore, Style
 from sys import exit, argv, stdout
 from time import strftime
-import subprocess
-import os
 
 class DownloadHandler(SimpleHTTPRequestHandler):
-    def end_headers(self):
-        filename = self.base_path.split('/')[-1]
-        self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
-        super().end_headers()
+	def end_headers(self):
+		filename=self.base_path.split('/')[-1]
+		self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
+		super().end_headers()
 
-    def log_message(self, format, *args):
-        client_ip = self.client_address[0]
-        current_time = strftime('%Y-%m-%d %H:%M:%S')
-        stdout.write(f'{Fore.RED}[paypy]{Style.RESET_ALL} {current_time} - {client_ip} - {format % args}\n')
-        stdout.flush()
-
-    def do_GET(self):
-        super().do_GET()
-        if self.path == '/':
-            # Intentar ejecutar el archivo
-            self.execute_file()
-
-    def execute_file(self):
-        try:
-            filename = self.base_path.split('/')[-1]
-            if os.name == 'nt':
-                subprocess.run([filename], shell=True)
-            else:  # Unix-based
-                subprocess.run(['chmod', '+x', filename])
-                subprocess.run([filename])
-            stdout.write(f'{Fore.GREEN}[paypy]{Style.RESET_ALL} Successfully executed {filename}\n')
-        except Exception as e:
-            stdout.write(f'{Fore.RED}[paypy]{Style.RESET_ALL} Failed to execute {filename}: {str(e)}\n')
-        stdout.flush()
+	def log_message(self, format, *args):
+		client_ip = self.client_address[0]
+		current_time = strftime('%Y-%m-%d %H:%M:%S')
+		stdout.write(f'{Fore.RED}[paypy]{Style.RESET_ALL} {current_time} - {client_ip} - {format % args}\n')
+		stdout.flush()
 
 def get_local_ip():
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.connect(('8.8.8.8', 80))
-    local_ip = s.getsockname()[0]
-    s.close()
-    return local_ip
+	s=socket(AF_INET,SOCK_DGRAM)
+	s.connect(('8.8.8.8',80))
+	local_ip=s.getsockname()[0]
+	s.close()
+	return local_ip
 
 # warn
 if len(argv) != 3:
-    print('usage: paypy <port> </payload/path>')
-    exit(1)
+	print('usage: paypy <port> </payload/path>')
+	exit(1)
 
 # run server
 port = int(argv[1])
 file_path = argv[2]
-DownloadHandler.base_path = file_path
-local_ip = get_local_ip()
-httpd = HTTPServer((local_ip, port), DownloadHandler)
+DownloadHandler.base_path=file_path
+local_ip=get_local_ip()
+httpd=HTTPServer((local_ip,port),DownloadHandler)
 print(f'{Fore.RED}[paypy]{Style.RESET_ALL} server started at: {local_ip}:{port}\n{Fore.RED}[paypy]{Style.RESET_ALL} downloadable payload: {file_path}')
 stdout.flush()
 httpd.serve_forever()
