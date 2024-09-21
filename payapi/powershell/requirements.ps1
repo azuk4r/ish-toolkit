b12d58661421bdd5391232b50ed5385150acd449
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-09-21 18:02:40 +0200

    requirements.ps1

---
+++ payapi/powershell/requirements.ps1
@@ -0,0 +1,13 @@
+$hwnd = Get-Process -Id $PID | ForEach-Object { $_.MainWindowHandle }
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class WinAPI {
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+"@
+[WinAPI]::ShowWindow($hwnd, 0)
+Start-Process powershell -ArgumentList "[System.Net.WebClient]::new().DownloadString('https://community.chocolatey.org/install.ps1') | Invoke-Expression" -WindowStyle Hidden -Wait
+Start-Process powershell -ArgumentList "choco install nmap --params='/NoZenmap' -y" -WindowStyle Hidden -Wait
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "Write-Host 'PowerShell has been restarted. Dependencies installed successfully.'; # Here an alert will be sent to the PayPy server: dependencies installed" -WindowStyle Normal
