29eafabcd003dfc0920db279fe6c1b1855f140ce
azuk4r <102126589+azuk4r@users.noreply.github.com>
2024-09-23 12:23:41 +0200

    working

--- payapi/powershell/1nstall.ps1
+++ payapi/powershell/1nstall.ps1
@@ -12,39 +12,48 @@ $opensshServer = Get-WindowsCapability -Online | Where-Object Name -like 'OpenSS
 if ($opensshServer.State -ne 'Installed') {
     Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 }
+Stop-Service sshd
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 $sshdConfigPath = "C:\ProgramData\ssh\sshd_config"
-if (-not (Test-Path $sshdConfigPath)) {
-    @"
+$sshdConfigBackupPath = "C:\ProgramData\ssh\sshd_config_backup"
+if (Test-Path $sshdConfigPath) {
+    Copy-Item $sshdConfigPath $sshdConfigBackupPath
+}
+@"
 Port 22
 AddressFamily any
 ListenAddress 0.0.0.0
 ListenAddress ::
 HostKey __PROGRAMDATA__/ssh/ssh_host_ed25519_key
 HostKey __PROGRAMDATA__/ssh/ssh_host_rsa_key
-AuthorizedKeysFile __PROGRAMDATA__/ssh/authorized_keys
+AuthorizedKeysFile C:/ProgramData/ssh/authorized_keys
 Subsystem sftp sftp-server.exe
 PubkeyAuthentication yes
 PasswordAuthentication no
 "@ | Set-Content $sshdConfigPath
-}
 Restart-Service sshd
 $firewallRule = Get-NetFirewallRule -DisplayName "Allow SSH"
 if (-not $firewallRule) {
     New-NetFirewallRule -Name "AllowSSH" -DisplayName "Allow SSH" -Protocol TCP -LocalPort 22 -Action Allow -Direction Inbound
 }
-$sshDir = "$HOME\.ssh"
+$sshDir = "C:\ProgramData\ssh"
 if (-not (Test-Path -Path $sshDir)) {
     New-Item -ItemType Directory -Force -Path $sshDir
 }
-icacls "$sshDir" /grant:r "Administradores:F" /T /C
-icacls "$sshDir\authorized_keys" /grant:r "Administradores:F" /C /T
 $sshKey = "$sshDir\id_rsa"
 $sshKeyPub = "$sshDir\id_rsa.pub"
-if (-not (Test-Path -Path $sshKey)) {
-    ssh-keygen -t rsa -b 2048 -f $sshKey -q -N ''
+if (Test-Path -Path $sshKey) {
+    Remove-Item $sshKey -Force
 }
+ssh-keygen -t rsa -b 2048 -f $sshKey -q
+$authorizedKeysPath = "$sshDir\authorized_keys"
+if (-not (Test-Path -Path $authorizedKeysPath)) {
+    New-Item -ItemType File -Force -Path $authorizedKeysPath
+}
+Add-Content -Path $authorizedKeysPath -Value (Get-Content -Path $sshKeyPub)
+icacls "$sshDir" /grant:r "Administradores:F" /T /C
+icacls "$authorizedKeysPath" /grant:r "Administradores:F" /C /T
 if (Test-Path -Path $sshKey) {
     $privateKey = Get-Content -Path $sshKey -Raw
 } else {
